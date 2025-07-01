71a1a5a9a0a788b60debc8df180c0ac4d8a66f35
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2025-07-01 12:39:28 +0700

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,12 @@ RUN apt update && apt install -y rsync
 # Copy setup script
 # Install dependencies initially to leverage Docker layer cache
 COPY . ./
+RUN rm -rf .git
+RUN npm install --omit=dev
 RUN chmod +x /app/setup.sh
-CMD ["/app/setup.sh"]
+RUN sh setup.sh
+
+WORKDIR /app
 
 # Copy entrypoint script
 RUN chmod +x /app/entrypoint.sh
--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "$(date +"%Y-%m-%d %H:%M:%S") | Syncing mounted source to /app..."
-find /app -mindepth 1 -maxdepth 1 ! -name 'node_modules' -exec rm -rf {} +
-rsync -av --exclude=node_modules /mnt/src/ /app/
+pwd
+ls
 
 cd /app
 
 echo "$(date +"%Y-%m-%d %H:%M:%S") | Installing dependencies..."
+rm -rf .git
 npm install --omit=dev
